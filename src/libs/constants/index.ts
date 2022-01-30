1eb795fb6eee3433e64c35f77a571e55672c155f
Shimpei Wakida <[shimpei.wakida@gmail.com>
2022-01-30 18:37:48 +0900

    環境構築完了

---
+++ src/libs/constants/index.ts
@@ -0,0 +1,25 @@
+export const color = {
+  green: "#4caf50",
+  red: "#f44336",
+  gray: "#9e9e9e",
+  white: "#fff",
+  black: "#000",
+} as const;
+
+export const radis = {
+  s: "4px",
+  m: "8px",
+  l: "16px",
+} as const;
+
+export const space = {
+  s: "4px",
+  m: "8px",
+  l: "16px",
+} as const;
+
+export const fontSize = {
+  s: "10px",
+  m: "16px",
+  l: "24px",
+} as const;
--- webpack.config.js
+++ webpack.config.js
@@ -4,18 +4,18 @@ const { resolve } = require("path");
 module.exports = {
   mode: "development",
   devtool: "inline-source-map",
-  entry: resolve(__dirname, "src/index.jsx"),
+  entry: resolve(__dirname, "src/index.tsx"),
   output: {
     filename: "index.js",
     path: resolve(__dirname, "dist"),
   },
   resolve: {
-    extensions: [".ts", ".js", ".jsx"],
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
   },
   module: {
     rules: [
       {
-        test: /\.(ts|jsx)/,
+        test: /\.(ts|tsx|jsx)/,
         use: {
           loader: "ts-loader",
         },
