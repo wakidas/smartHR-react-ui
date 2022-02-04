1fa1db0e668e70437ab256b25da3b7dc50ee4191
Shimpei Wakida <[shimpei.wakida@gmail.com>
2022-02-04 23:30:39 +0900

    Buttonコンポーネントに各class名を反映できるように

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Button } from "./libs/Button";
 import { Text } from "./libs/Text";
 import { Heading } from "./libs/Heading";
 
@@ -5,6 +6,22 @@ export const App = () => {
   return (
     <>
       <Text text="true" />;<Heading tag="h1">見出し</Heading>
+      <Button
+        onClick={() => console.log("clicked")}
+        title="Button"
+        type="primary"
+        width={96}
+      />
+      <Button
+        onClick={() => console.log("clicked")}
+        title="Button"
+        type="secondary"
+      />
+      <Button
+        onClick={() => console.log("clicked")}
+        title="Button"
+        type="error"
+      />
       <Heading tag="h1">
         <span> hello world!</span>
       </Heading>
